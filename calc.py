9cf0b22cf758c7fb8173538f2732e706816e8d09
ard303 <ayansajjad1010@gmail.com>
2025-02-18 21:39:35 +0500

    python

---
+++ calc.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+print("the sum of the two numbers is", add(a,b))
+print("the sub of the two numbers is", sub(a,b))
+print("the multiplication of the two numbers is", mul(a,b))
+print("the division of the two numbers is", div(a,b))
