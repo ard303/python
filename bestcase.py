8bb9449e5d2fe0e4181e08c8a0bc8a9e3b313f64
ard303 <ayansajjad1010@gmail.com>
2025-04-17 21:24:41 +0500

    constanttimecomplexibility

---
+++ bestcase.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+
+    iteration=0
+    print("the number entered by the user is", n )
+    iteration=+1
+    print("the total iterations done by the code is", iteration)
+
+printnumber(20)
